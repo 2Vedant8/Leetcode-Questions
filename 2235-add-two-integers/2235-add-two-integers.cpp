ee9f417d9fd36d41b100b9b9f40bb8d48a9bc4ea
Vedant Gupta <vanshgupta028@gmail.com>
2023-04-02 18:11:22 +0530

    Time: 0 ms (100.00%), Space: 6 MB (64.06%) - LeetHub

--- 2235-add-two-integers/2235-add-two-integers.cpp
+++ 2235-add-two-integers/2235-add-two-integers.cpp
@@ -1,7 +1,13 @@
 class Solution {
 public:
-    int sum(int num1, int num2) {
-        return num1+num2;
-        
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        sort(potions.begin(), potions.end());
+        vector<int> res;
+        for (int spell: spells) {
+            long potion_needed = (success + spell - 1) / spell;
+            auto it = lower_bound(potions.begin(), potions.end(), potion_needed);
+            res.push_back(potions.end() - it);
+        }
+        return res;
     }
 };
