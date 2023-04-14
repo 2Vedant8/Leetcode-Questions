02649f19f732c2894ba213a0905ae0604767180c
Vedant Gupta <vanshgupta028@gmail.com>
2023-04-14 17:37:07 +0530

    Time: 2698 ms (29.61%), Space: 6 MB (6.91%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int ans = 0;
+        for(int i=1; i<num; i++)
+        {
+            if(num%i==0)
+            {
+                ans = ans + i;
+            }
+        }
+            if(ans==num)
+            {
+                return true;
+            }
+        return false;
+    }
+};
