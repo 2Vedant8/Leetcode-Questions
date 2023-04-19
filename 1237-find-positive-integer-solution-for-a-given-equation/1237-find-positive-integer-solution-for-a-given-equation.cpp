c3e760cc410f8f608bbd8e550d8643d2999541db
Vedant Gupta <vanshgupta028@gmail.com>
2023-04-20 00:08:49 +0530

    Time: 7 ms (24.95%), Space: 6.4 MB (27.72%) - LeetHub

--- 1237-find-positive-integer-solution-for-a-given-equation/1237-find-positive-integer-solution-for-a-given-equation.cpp
+++ 1237-find-positive-integer-solution-for-a-given-equation/1237-find-positive-integer-solution-for-a-given-equation.cpp
@@ -10,28 +10,58 @@
  * };
  */
 
+// class Solution {
+// public:
+//     vector<vector<int>> findSolution(CustomFunction& customfunction, int z) {
+
+//         vector<vector<int>>ans;
+//         for(int i=1; i<=1000; i++)
+//         {
+//             for(int j=1; j<=1000; j++)
+//             {
+//                 vector<int>temp;
+//                 if(customfunction.f(i,j)==z)
+//                 {
+//                     temp.push_back(i);
+//                     temp.push_back(j);
+//                     ans.push_back(temp);
+//                 }
+//                 else if(customfunction.f(i,j)>z)
+//                 {
+//                     break;
+//                 }
+//             }
+//         }
+//             return ans;
+//     }
+// };
+// ////////////////////////////////////////////////////////////////
 class Solution {
 public:
     vector<vector<int>> findSolution(CustomFunction& customfunction, int z) {
-
         vector<vector<int>>ans;
-        for(int i=1; i<=1000; i++)
+        
+        for(int i=1;i<=1000;i++)
         {
-            for(int j=1; j<=1000; j++)
+            int left=1,right=1000;
+            
+            while(left<=right)
             {
-                vector<int>temp;
-                if(customfunction.f(i,j)==z)
+                int mid=(left+right)/2;
+                if(customfunction.f(i,mid)==z)
                 {
+                    vector<int>temp;
                     temp.push_back(i);
-                    temp.push_back(j);
+                    temp.push_back(mid);
                     ans.push_back(temp);
-                }
-                else if(customfunction.f(i,j)>z)
-                {
                     break;
                 }
+                else if(customfunction.f(i,mid)>z)
+                    right=mid-1;
+                else
+                    left=mid+1;
             }
         }
-            return ans;
+        return ans;
     }
 };
