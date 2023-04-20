f770fc1f41cf79d97441c0e5b1debbbfa9e70a3b
Vedant Gupta <vanshgupta028@gmail.com>
2023-04-20 22:38:31 +0530

    Time: 3 ms (68.85%), Space: 8.6 MB (50.18%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end());
+        int n = citations.size();
+        int start = 0;
+        int end = n-1;
+        int mid = start + (end-start)/2;
+        while(start<=end)
+        {
+            if(citations[mid]==(n-mid))
+            {
+                return citations[mid];
+            }
+            else if(citations[mid]>(n-mid))
+            {
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+            mid = start + (end-start)/2;
+        }
+        return n-start;
+
+
+        
+    }
+};
