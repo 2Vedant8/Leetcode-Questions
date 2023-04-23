37648d280cc73c3e0bf4cd3fcff0e0ace63f8e64
Vedant Gupta <vanshgupta028@gmail.com>
2023-04-23 23:01:21 +0530

    Time: 26 ms (37.29%), Space: 23.3 MB (50.31%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
-    void reverseString(vector<char>& s) {
-        int start =0;
-        int end = s.size()-1;
-        while(start<=end)
-        {
-            swap(s[start] , s[end]);
-            start++;
-            end--;
+    void recursion(vector<char>& s, int start, int end) {
+        // Base case
+        if (start >= end) {
+            return;
         }
+        // Swap first and last characters
+        swap(s[start], s[end]);
+        // Recurse on the remaining substring
+        recursion(s, start + 1, end - 1);
+    }
+    void reverseString(vector<char>& s) {
+        
+        // int start =0;
+        // int end = s.size()-1;
+        // while(start<=end)
+        // {
+        //     swap(s[start] , s[end]);
+        //     start++;
+        //     end--;
+        // }
+        
+        recursion(s, 0, s.size()-1);
+        
+       
         
     }
 };
