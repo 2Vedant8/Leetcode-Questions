3c2887a14ad3794e75d0385ccdd5b7ed13c556bd
Vedant Gupta <vanshgupta028@gmail.com>
2023-08-07 00:51:35 +0530

    Time: 7 ms (39.06%), Space: 20.4 MB (35.53%) - LeetHub

---
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.cpp
@@ -0,0 +1,11 @@
+class Solution
+{
+    public:
+        int numberOfEmployeesWhoMetTarget(vector<int> &hours, int target)
+        {
+            int ans = 0;
+            for (auto h: hours) ans += (h >= target);
+            return ans;
+        }
+
+};
